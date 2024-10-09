eb25f2b97e32402f4f5a050fe82dbc4f3975ba40
Premiumpumpkin <toniototo10@gmail.com>
2024-10-08 23:44:48 -0400

    Update HW3.sql

--- HW3/HW3.sql
+++ HW3/HW3.sql
@@ -16,11 +16,23 @@ VALUES
 ('Remi', 'Denvor', 'CO');
 
 CREATE TABLE IF NOT EXISTS products(
-pid INT UNSIGNED PRIMARY KEY,
+pid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(50),
 category VARCHAR(50),
 description VARCHAR(255)
 );
+
+INSERT INTO products(name, category, description)
+VALUES
+('Printer', 'Computer', 'A device that prints virtual images on pieces of paper'),
+('Ethernet Adapter', 'Converts Ethernet wire into USB-A wired connection'),
+('Desktop', 'Computer', 'A stand-alone computer, usually really powerful but requires a keyboard, a mouse, and monitor'),
+('Hard Drive', 'Computer', 'Either an SSD or HDD, used to store data in either Desktops or Laptops'),
+('Router', 'Networking', 'Used for generating Wi-Fi connections for computers, usually requires a membership'),
+('Network Card', 'Networking', 'A card for computers that allows to send and recieve Wi-Fi connections')
+('Super Drive', 'Computer', 'A drive that allows for DVD/CDs to be inserted into and accessed via a computer'),
+('Laptop', 'Computer', 'A computer that comes built in with a monitor, a mouse-like pad known a s a trackpad, and a keyboard!'),
+('Monitor', 'Peripheral', 'A screen that can be attached to a computer to gain visual access to either a Desktop or a Laptop');
 
 CREATE TABLE IF NOT EXISTS sell(
 mid INT UNSIGNED,
