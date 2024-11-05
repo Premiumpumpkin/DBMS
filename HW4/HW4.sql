b67df44fda70ac5f22a882adb657eb3d3c1e80c6
Premiumpumpkin <toniototo10@gmail.com>
2024-11-05 02:19:35 -0500

    Everything

--- HW4/HW4.sql
+++ HW4/HW4.sql
@@ -79,6 +79,12 @@ CREATE TABLE IF NOT EXISTS film (
     FOREIGN KEY (language_id) REFERENCES language(language_id)
 );
 
+CREATE TABLE IF NOT EXISTS actor (
+    actor_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS film_actor (
     actor_id INT,
     film_id INT,
@@ -128,12 +134,6 @@ CREATE TABLE IF NOT EXISTS payment (
     FOREIGN KEY (rental_id) REFERENCES rental(rental_id)
 );
 
-CREATE TABLE IF NOT EXISTS actor (
-    actor_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(45) NOT NULL,
-    last_name VARCHAR(45) NOT NULL
-);
-
 #Queriers
 #1
 SELECT c.name AS category,
@@ -159,12 +159,12 @@ WHERE avg_length = (SELECT MAX(avg_length) FROM category_avg_length) OR avg_leng
 #3
 SELECT cu.customer_id, cu.first_name, cu.last_name
 FROM customer cu
-JOIN rental r ON cu.customer_id = r.customer_id
+JOIN rental r ON cu.customer_id = r.customer_id #joining customers, inventory, and film categories together to validate them
 JOIN inventory i ON r.inventory_id = i.inventory_id
 JOIN film_category fc ON i.film_id = fc.film_id
 JOIN category c ON fc.category_id = c.category_id
-WHERE c.name = 'Action' AND cu.customer_id NOT IN(
-	SELECT cu2.customer_id
+WHERE c.name = 'Action' AND cu.customer_id NOT IN( #essentially checking for action movies rented and NOT comedy/classics
+	SELECT cu2.customer_id #below is just the same thing up top just doing the opposite (making sure customers do NOT have comedy or classics movies rented)
     FROM customer cu2
     JOIN rental r2 ON cu2.customer_id = r2.customer_id
     JOIN inventory i2 ON r2.inventory_id = i2.inventory_id
@@ -174,36 +174,36 @@ WHERE c.name = 'Action' AND cu.customer_id NOT IN(
     );
 
 #4
-SELECT a.actor_id, a.first_name, a.last_name, COUNT(*) AS film_count
+SELECT a.actor_id, a.first_name, a.last_name, COUNT(*) AS film_count #counting the amount of times an actor appears period.
 FROM actor a
-JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film_actor fa ON a.actor_id = fa.actor_id 
 JOIN film f ON fa.film_id = f.film_id
 JOIN language l ON f.language_id = l.language_id
-WHERE l.name = 'English'
+WHERE l.name = 'English' #filtering for movies to be English language
 GROUP BY a.actor_id, a.first_name, a.last_name
-ORDER BY film_count DESC
-LIMIT 1;
+ORDER BY film_count DESC #ordering by most appearances
+LIMIT 1; #showing ONLY the most appeared actor.
 
 #5
-SELECT COUNT(DISTINCT i.film_id) AS distinct_movies_10_days
+SELECT COUNT(DISTINCT i.film_id) AS distinct_movies_10_days #getting a count on all distinct movies
 FROM rental r
 JOIN staff s ON r.staff_id = s.staff_id
 JOIN inventory i ON r.inventory_id = i.inventory_id
-WHERE DATEDIFF(r.return_date, r.rental_date) = 10 AND s.first_name = 'Mike';
+WHERE DATEDIFF(r.return_date, r.rental_date) = 10 AND s.first_name = 'Mike'; #this is where we then check to see if rental date - return date is equal to 10 days, and if it was mike who done it.
 
 #6
-WITH movie_actor_count AS(
+WITH movie_actor_count AS( #collecting a list of movies with their actor counts
 SELECT f.film_id, COUNT(fa.actor_id) AS actor_count
     FROM film f
     JOIN film_actor fa ON f.film_id = fa.film_id
 	GROUP BY f.film_id
 )
-SELECT a.first_name, a.last_name
+SELECT a.first_name, a.last_name #selecting our first name and last name
 FROM actor a
 JOIN film_actor fa ON a.actor_id = fa.actor_id
-WHERE fa.film_id = (
+WHERE fa.film_id = (#This is where we select the film with the highest actor count
 	SELECT film_id
     FROM movie_actor_count
-    ORDER BY actor_count DESC LIMIT 1
+    ORDER BY actor_count DESC LIMIT 1 #we Limit to 1 and descending to get essentially our highest acount count here
 )
-ORDER BY a.first_name, a.last_name;
+ORDER BY a.first_name, a.last_name; #ordering by alphabetical
